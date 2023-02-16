66f4b5b61209964d2b63e0d1ee9461513bb1584c
AlejandroLilloRodriguez <alejandro.lillorodriguez@usp.ceu.es>
2023-02-16 16:56:49 +0100

    ejercicio 11 terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,11 @@
-package aplicacion;public class Principal {
+package aplicacion;
+
+import dominio.Ejercicio11;
+
+public class Principal {
+    public static void main(String[]args){
+        int [] lista= {1,2,3};
+        int [] lista2= {2,4,6};
+        System.out.print(" "+ Ejercicio11.calcularProductoEscalar(lista,lista2,0));
+    }
 }
--- src/dominio/Ejercicio11.java
+++ src/dominio/Ejercicio11.java
@@ -1,2 +1,12 @@
-package dominio;public class Ejercicio11 {
+package dominio;
+
+public class Ejercicio11 {
+    public static int calcularProductoEscalar(int[]lista,int []lista2,int n){
+        if(n>lista.length-1){
+            return 0;
+        }else{
+            return lista[n]*lista2[n]+calcularProductoEscalar(lista,lista2,n+1);
+        }
+
+    }
 }
